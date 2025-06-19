eca1d6993cea0bf60c4a9cdb803c56d03cb4a946
Abdul Rahman Taqesh <52210903+Abbo0dio@users.noreply.github.com>
2025-06-19 12:49:08 +0400

    Update and rename schema.sql to ignore.txt

--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE internship_portal;
-USE internship_portal;
-
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255),
-  password VARCHAR(255),
-  role ENUM('student', 'company')
-);
